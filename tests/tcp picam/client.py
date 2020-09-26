d86cbbe5e83928da47203c87d38cd378d81e55ea
Jacob Cassady <jtcass01@louisville.edu>
2020-09-25 21:39:50 -0400

    Trying an implementation of vidgear for tcp communication of pycamera

--- tests/tcp picam/client.py
+++ tests/tcp picam/client.py
@@ -1,6 +1,40 @@
 ##!/usr/bin/env python
 """
 """
+from vidgear.gears import NetGear
+import cv2
+
+if __name__ == "__main__":
+    # define netgear client with `receive_mode = True` and default settings
+    client = NetGear(address="192.168.50.76", receive_mode=True)
+
+    # infinite loop
+    while True:
+        # receive frames from network
+        frame = client.recv()
+
+        # check if frame is None
+        if frame is None:
+            # if True break the infinite loop
+            break
+
+        # do something with frame here
+
+        # Show output window
+        cv2.imshow("Output Frame", frame)
+
+        key = cv2.waitKey(1) & 0xFF
+        # check for 'q' key-press
+        if key == ord("q"):
+            # if 'q' key-pressed break out
+            break
+
+    # close output window
+    cv2.destroyAllWindows()
+    # safely close client
+    client.close()
+
+"""
 from imutils.video import VideoStream
 import imagezmq
 import argparse
@@ -26,3 +60,4 @@ if __name__ == "__main__":
     while True:
         frame = vs.read()
         sender.send_image(rpiName, frame)
+"""
--- tests/tcp picam/server.py
+++ tests/tcp picam/server.py
@@ -1,7 +1,44 @@
 ##!/usr/bin/env python
 """
+example taken from: https://abhitronix.github.io/vidgear/gears/netgear/usage/
 """
+from vidgear.gears import VideoGear
+from vidgear.gears import NetGear
 
+if __name__ == "__main__":
+    # open any valid video stream(for e.g `test.mp4` file)
+    stream = VideoGear(enablePiCamera=True, source='test.mp4').start()
+
+    # Define Netgear Server with default parameters
+    server = NetGear()
+
+    # loop over until KeyBoard Interrupted
+    while True:
+
+        try:
+
+            # read frames from stream
+            frame = stream.read()
+
+            # check for frame if Nonetype
+            if frame is None:
+                break
+
+            # {do something with the frame here}
+
+            # send frame to server
+            server.send(frame)
+
+        except KeyboardInterrupt:
+            break
+
+    # safely close video stream
+    stream.stop()
+
+    # safely close server
+    server.close()
+
+"""
 from imutils import build_montages
 from datetime import datetime
 import numpy as np
@@ -29,3 +66,4 @@ if __name__ == "__main__":
         cv2.putText(frame, rpiName, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
         cv2.imshow("Frame", frame)
+"""
